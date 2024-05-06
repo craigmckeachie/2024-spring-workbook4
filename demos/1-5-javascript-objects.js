5ffbcba2be76b7154a35810cafffb89309d53e6e
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-06 10:21:50 -0400

    object demo

---
+++ demos/1-5-javascript-objects.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let employee = {
+  employeeId: "A134L",
+  name: "Ezra",
+  jobTitle: "Theater Teacher",
+  payRate: 38.46,
+};
+console.log(employee.name,employee.jobTitle, employee.payRate);
+
