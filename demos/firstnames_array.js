3a283843cf8078537ce7cd9a425ad629cf313216
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-07 11:51:21 -0400

    firstnames array

--- demos/firstnames_array.js
+++ demos/firstnames_array.js
@@ -1,9 +1,13 @@
 "use strict";
 
-let firstNames = ["Nedret", "Chris", "Nathaniel", "Yusuf"];
+let firstNames = ["Nedret", "Chris", "Nathaniel", "Yusuf", "Scott", "Erik", "Denzel"];
 // console.log(firstNames[1]); //Chris
 // console.log(firstNames.length);
 
 for (let index = 0; index < firstNames.length; index++) {
     console.log(firstNames[index]);
+}
+
+for (let firstName of firstNames) {
+    console.log(firstName);
 }
