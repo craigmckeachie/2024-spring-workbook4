57589e4d55cd0868a0b6bf393f79e1d81ecfc576
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-07 10:23:54 -0400

    3 parts

---
+++ demos/while-3-parts.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let lap = 1; //1. code that executes before the loop begins
+
+//2. lap<=500 is the condition that must be true in order for the loop to keep executing
+while (lap <= 500) {
+  console.log("Car is on lap: " + lap);
+  lap = lap + 1; //lap++ | 3. code that runs at the bottom of each iteration
+}
