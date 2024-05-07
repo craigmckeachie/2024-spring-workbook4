f6aec7e1b3783f54d8eb4a9497b537cdae9183e2
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-07 11:32:44 -0400

    firstnames loop print

--- demos/firstnames_array.js
+++ demos/firstnames_array.js
@@ -1,4 +1,9 @@
 "use strict";
 
 let firstNames = ["Nedret", "Chris", "Nathaniel", "Yusuf"];
-console.log(firstNames[1]); //Chris
+// console.log(firstNames[1]); //Chris
+// console.log(firstNames.length);
+
+for (let index = 0; index < firstNames.length; index++) {
+    console.log(firstNames[index]);
+}
