0fe2a92010952f2f358b1e3cd4a548120ec7edf9
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-07 09:25:12 -0400

    add comments rename variable

--- demos/2-4-while-loop.js
+++ demos/2-4-while-loop.js
@@ -8,8 +8,11 @@ let numberOfTimesThroughLoop = 1;
 // this one too
 // increment the counter, add one to the counter so the condition is eventually met and the loop ends
 //}
-while (numberOfTimesThroughLoop < 5) {
+while (numberOfTimesThroughLoop <= 5) {
   number = number * 2;
   console.log(number);
   numberOfTimesThroughLoop = numberOfTimesThroughLoop + 1;
 }
+
+
+
