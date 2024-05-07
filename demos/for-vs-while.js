aa3ef3743983b180dfd6c1a55d3fbae8c0183809
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-07 11:16:34 -0400

    for and while exercises

--- demos/for-vs-while.js
+++ demos/for-vs-while.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//while
+//####################
+
 let lap = 1; //1. code that executes before the loop begins
 
 //2. lap<=500 is the condition that must be true in order for the loop to keep executing
@@ -7,6 +10,10 @@ while (lap <= 500) {
   console.log("Car is on lap: " + lap);
   lap = lap + 1; //lap++ | 3. code that runs at the bottom of each iteration
 }
+
+
+//for loop
+//##############################
 
 for (let lap = 1; lap <= 500; lap = lap + 1) {
   console.log("Car is on lap: " + lap);
---
+++ exercises/for_loops.js
@@ -0,0 +1,5 @@
+"use strict";
+
+for (let count = 1; count <= 7; count++) {
+  console.log("I love loops!");
+}
