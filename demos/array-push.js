b05405999bcbdee4ab5214fb6cf380dfcf971dde
Craig McKeachie <craigdmckeachie@gmail.com>
2024-05-08 10:07:52 -0400

    array push

---
+++ demos/array-push.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let numbers = [1,2,3]
+numbers.push(4); //adds item to the end of the array
+console.log(numbers);
+
+let names = [];
+names.push("Aung");
+names.push("Pooja");
+names.push("Roshan");
+console.log(names);
+
+
